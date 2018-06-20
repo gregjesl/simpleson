c6ef4610787d3431c761297de8f4a1841450d153
Gregory Eslinger <gregjesl@gmail.com>
2018-06-20 20:11:55 +0200

    Fixed ambigious overload issue

--- test/jarray.cpp
+++ test/jarray.cpp
@@ -4,12 +4,12 @@
 int main(void)
 {
 	json::jarray jresult = json::jarray::parse(" [ 123 , 456 , 789 ]");
-	std::vector<int> result = (std::vector<int>)jresult;
+	std::vector<int> result = jresult;
 	assert(result[0] == 123);
 	assert(result[1] == 456);
 	assert(result[2] == 789);
 
-	result = (std::vector<int>)json::jarray::parse((std::string)jresult);
+	result = json::jarray::parse((std::string)jresult);
 	assert(result[0] == 123);
 	assert(result[1] == 456);
 	assert(result[2] == 789);
