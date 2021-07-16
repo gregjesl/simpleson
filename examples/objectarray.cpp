2e659c71f8830098e41749c9f88a562f464bb117
Gregory Eslinger <gregjesl@gmail.com>
2021-07-15 21:30:59 -0600

    Added examples directory

---
+++ examples/objectarray.cpp
@@ -0,0 +1,35 @@
+#include "json.h"
+#include <stdio.h>
+#include <assert.h>
+
+int main(void)
+{
+	// Create a couple objects
+    json::jobject obj1 = json::jobject::parse("{\"hello\":\"world\"}");
+    json::jobject obj2;
+    obj2["key"] = "value";
+
+    // Put the objects in a vector
+    std::vector<json::jobject> vec;
+    vec.push_back(obj1);
+    vec.push_back(obj2);
+
+    // Create an object and add the array
+    json::jobject example;
+    example["array"] = vec;
+
+    // Serialize the object
+    std::string serial = (std::string)example;
+
+    // Print the result
+    printf("%s\n", serial.c_str());
+
+    // Read back the items
+    std::vector<json::jobject> readbackarray = example["array"];
+
+    // Access each item
+    json::jobject readback1 = readbackarray[0];
+    assert(readback1 == obj1);
+    json::jobject readback2 = readbackarray[1];
+    assert(readback2 == obj2);
+}
