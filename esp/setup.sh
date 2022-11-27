e6cb9c39e19b7abe759be2fe5a1bea954311b962
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 09:09:11 -0700

    Moved from virtual environment to alias

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash 
+which python
+alias python=/usr/bin/python3.10
 apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 # Check the current version of python
-python --version
-. $HOME/venv3.11/bin/activate
-type python
-# Should be python v3.X
 python --version
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
