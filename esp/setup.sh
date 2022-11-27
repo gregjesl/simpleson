cb8c7f8a00fc390b87731917e2ec8051cc8dd513
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 16:47:24 -0700

    Moved to different IDF for debugging

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,9 +1,7 @@
-#!/usr/local/bin/python3.10
-# Check the current version of python
-python --version
+#!/bin/bash
 # ESP idf dependencies
 apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
-git clone --recursive https://github.com/espressif/esp-idf.git
+git clone --recursive https://github.com/gregjesl/esp-idf.git
 cd esp-idf
 ./install.sh esp32
 . ./export.sh
