64b2a2ed1a2d345e48911ca049fb7011eaa78a0f
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 03:20:31 -0700

    Different approach for Python3

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash 
 apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
-update-alternatives --install /usr/bin/python /usr/bin/python3 1
+# Check the current version of python
+python --version
+python3 -m venv myenv
+. ./myenv/bin/activate
+type python
+# Should be python v3.X
+python --version
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 ./install.sh esp32
