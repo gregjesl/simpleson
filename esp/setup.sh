61919be047bd49ddbc2bdf1ff9bcdaf112567dcb
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 12:46:52 -0700

    Different approach to pyenv

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash 
-which python
-alias python=/usr/bin/python3.10
-apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+# Pyenv dependencies
+apt-get install -y make build-essential git libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+curl https://pyenv.run | bash
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+exec "$SHELL"
+pyenv versions
+pyenv install 3.10.8
 # Check the current version of python
 python --version
+# ESP idf dependencies
+apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 ./install.sh esp32
