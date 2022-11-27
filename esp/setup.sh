661082b06887df2d8779640ce7d6bf14b814f9ce
Gregory Eslinger <gregjesl@gmail.com>
2022-11-26 21:54:36 -0700

    Fixed Python 3 support

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+apt-get install -y git wget flex bison gperf python3 python-is-python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 ./install.sh esp32
