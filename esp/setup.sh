4e00d7bef758491f9556d172b4620dfb7edc4c22
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 03:09:28 -0700

    Fixed python 2.7 issue

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
-apt-get install -y git wget flex bison gperf python3 python-is-python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+update-alternatives --install /usr/bin/python /usr/bin/python3 1
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 ./install.sh esp32
