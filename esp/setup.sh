436c93527e50c2a99882050fee01b7db140c9999
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 17:34:19 -0700

    Added whereis to find Python

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # ESP idf dependencies
+whereis python3.7
 apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 git clone --recursive https://github.com/gregjesl/esp-idf.git
 cd esp-idf
