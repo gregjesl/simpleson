5106bcaedc7434e8e589b628ec8dd5e9920a22fc
Gregory Eslinger <gregjesl@gmail.com>
2022-11-27 20:41:36 -0700

    wget specific python

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Install Python 3.7.15
+wget https://www.python.org/ftp/python/3.7.15/Python-3.7.15.tgz
+tar -xvf Python-3.7.15.tgz
+cd Python-3.7.15
+./configure --enable-optimizations
+make
+make install
+cd ~
 # ESP idf dependencies
-whereis python3.7
 apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 git clone --recursive https://github.com/gregjesl/esp-idf.git
 cd esp-idf
