2063d0aed29059bc614901738cf6dac712a2eac1
Gregory Eslinger <gregjesl@gmail.com>
2022-12-01 06:51:17 -0700

    Reordered build

--- esp/setup.sh
+++ esp/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Install Python 3.7.15
+apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+cd ~
 wget https://www.python.org/ftp/python/3.7.15/Python-3.7.15.tgz
 tar -xvf Python-3.7.15.tgz
 cd Python-3.7.15
@@ -8,7 +10,6 @@ make
 make install
 cd ~
 # ESP idf dependencies
-apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 ./install.sh esp32
