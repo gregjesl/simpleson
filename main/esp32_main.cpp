243a064c7140bd2b586cfbbc97f4ceb48ec98d01
Gregory Eslinger <gregjesl@gmail.com>
2022-12-03 10:16:22 -0700

    Updated printout

--- main/esp32_main.cpp
+++ main/esp32_main.cpp
@@ -6,45 +6,35 @@
 #include "esp_flash.h"
 #include "json.h"
 
-static unsigned int ticks = 0;
-void tick()
+
+void print_info()
 {
-    while(true)
-    {
-        json::jobject result;
-        result["ticks"] = ticks++;
-        printf("%s\n", result.as_string().c_str());
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    esp_chip_info_t chip_info;
+    uint32_t flash_size = 0;
+    esp_chip_info(&chip_info);
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
     }
+
+    json::jobject silicon_rev, flash, heap, result;
+    result["chip"] = CONFIG_IDF_TARGET;
+    result["cores"] = chip_info.cores;
+    result["BT"].set_boolean((chip_info.features & CHIP_FEATURE_BT));
+    result["BTE"].set_boolean((chip_info.features & CHIP_FEATURE_BLE));
+    result["LR-WPAN"].set_boolean((chip_info.features & CHIP_FEATURE_IEEE802154));
+    silicon_rev["major"] = chip_info.revision / 100;
+    silicon_rev["minor"] = chip_info.revision % 100;
+    result["silicon"] = silicon_rev;
+    flash["size"] = flash_size / (1024 * 1024);
+    flash["type"] = (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external";
+    result["flash"] = flash;
+    heap["free"] = esp_get_free_heap_size();
+    heap["minimum"] = esp_get_minimum_free_heap_size();
+    result["heap"] = heap;
+    printf("%s\n", result.as_string().c_str());
 }
 
 extern "C" void app_main()
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%uMB %s flash\n", flash_size / (1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    tick();
+    print_info();
 }
