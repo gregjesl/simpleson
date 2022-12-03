ab93576d2574f444ac025a151d1f0a025d64638a
Gregory Eslinger <gregjesl@gmail.com>
2022-12-03 07:54:31 -0700

    Updated readme

--- esp/setup.sh
+++ esp/setup.sh
@@ -2,12 +2,6 @@
 # Install Python 3.7.15
 apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 cd ~
-wget https://www.python.org/ftp/python/3.7.15/Python-3.7.15.tgz
-tar -xvf Python-3.7.15.tgz
-cd Python-3.7.15
-./configure
-make
-make install
 cd ~
 # ESP idf dependencies
 git clone --recursive https://github.com/espressif/esp-idf.git
